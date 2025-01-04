a09b8db82ecfe7a51473e467c77e121c013441a9
PardusDrain <zerozet100@gmail.com>
2025-01-04 17:04:16 +0800

    modified structure project

---
+++ App.jsx
@@ -0,0 +1,16 @@
+import React, { useState } from 'react'
+import Header from './components/Header/Header.jsx'
+import Footer from './components/Footer/Footer.jsx'
+import Main from './components/Main/Main.jsx'
+
+export default function App() {
+  return (
+    <>
+    <Header/>
+    <Main/>
+    <Footer/>
+    </>
+  )
+}
+
+
---
+++ components/Header/Registration/Registration.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import './Registration.css'
+
+export default function Registration() {
+  return (
+    <>
+    </>
+  )
+}
